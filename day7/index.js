7c7764f969909e5072f246ba733d5a3a3f154c9a
jjekoo <jekow271299@gmail.com>
2018-11-09 15:56:40 +0700

    merge file index.js & index2.js

--- day7/index.js
+++ day7/index.js
@@ -18,4 +18,29 @@ if (name) {
     console.log("Anda tidak boleh masuk!");
 }
 
+/* CARA KEDUA */
 
+var name;
+var age;
+var money;
+var juice = 50000;
+var anggur = 300000;
+
+
+if (name) {
+    if (age < 17) {
+        var change = money - juice;
+        if (money >= juice) {
+          console.log ("Anda bisa pesan minum. Sisa uang anda Rp." + change)
+        } else { console.log("uang tidak cukup")
+        }
+    } else if (age >= 17) {
+        var change = money - anggur;
+        if (money >= anggur) {
+          console.log ("Anda bisa pesan minum. Sisa uang anda Rp." + change)
+        } else { console.log("uang tidak cukup")
+        }
+    }
+} else {
+    console.log("Anda tidak boleh masuk!");
+}
